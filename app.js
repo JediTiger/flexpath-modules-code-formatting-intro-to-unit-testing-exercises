891daed57dafd39df70de7207ace96fb043c9caf
Sean Simpson <JediTiger@users.noreply.github.com>
2026-07-06 11:35:48 -0500

    exercise 1 and 2 completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {add, multiply} from './mathUtils.js';
+import { ltc } from './logToConsole.js';
 
 ltc(`The sum of 5 and 3 is: ${add(5, 3)}`);
 ltc(`The product of 5 and 3 is: ${multiply(5, 3)}`);
